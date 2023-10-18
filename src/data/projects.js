c445dfb838d7864159674a77f5528402eff16948
Aman Thakkar <aman.thakkar@somaiya.edu>
2023-10-18 10:26:18 -0700

    fixed some puncuations

--- src/data/projects.js
+++ src/data/projects.js
@@ -9,8 +9,8 @@ const data = [
     desc:
       "A full-stack website showing what you're reading here, including my Bitcoin indicator.  Front-end is hosted using Github Pages. " +
       'The unique visits count is done through a self-hosted backend on AWS with containerized Node and Redis. ' +
-      'Python script for the indicator runs daily on the same server, with the image uploaded to a public S3 bucket.' +
-      "I earlier used AWS Lambda with CloudWatch but it wasn't feasible in the free tier so now it's just a cron job. " +
+      'Python script for the indicator runs daily on the same server, with the image uploaded to a public S3 bucket. ' +
+      "I tried using AWS Lambda with CloudWatch and layers using Cloud9 but it wasn't feasible in the free tier so now it's just a cron job :D. " +
       "I've also used Nginx as a reverse proxy and Certbot for enabling HTTPS communication. " +
       'Domain and subdomain purchased through Squarespace. ',
   },
@@ -23,7 +23,7 @@ const data = [
     desc:
       'Designed, developed, and deployed a cross-platform mobile app using React Native for cost-effective, scheduled grocery subscriptions and deliveries. ' +
       'We incorporated REST APIs with a Node.js backend and used efficient state management on the frontend for extremely smooth cart manipulation and order processing. ' +
-      'Backend was deployed to an EC2 instance using Docker, by leveraging an automated workflow powered by GitHub Actions. Click on the image to view our Github repo and associated vidoes',
+      'Backend was deployed to an EC2 instance using Docker, by leveraging an automated workflow powered by GitHub Actions. Click on the image to view our Github repo and associated vidoes! ',
   },
   {
     title: 'Financial Analysis of Cryptocurrencies',
@@ -56,7 +56,7 @@ const data = [
     date: '2021-12-15',
     desc:
       'Analyzed liquidity pools and protocols for lending and borrowing on Decentralized Exchanges. ' +
-      'We used flash loans to implement a two-step fixed spread liquidation of an overcollateralized on-chain loan.',
+      'We used flash loans to implement a two-step fixed spread liquidation of an overcollateralized on-chain loan. ',
   },
   {
     title: 'Counterfeit Detection in Supply Chains using Blockchain',
@@ -66,7 +66,7 @@ const data = [
     date: '2019-05-15',
     desc:
       'Studied Andreas Antonopoulos\' "Mastering Bitcoin" to understand how Bitcoin enables "Proof Of Possession" of funds  . ' +
-      'Our team of 4 then created our own version of a Bitcoin-like blockchain, entirely from scratch in Golang, to mimic supply chain transactions to prevent counterfeits',
+      'Our team of 4 then created our own version of a Bitcoin-like blockchain, entirely from scratch in Golang, to mimic supply chain transactions to prevent counterfeits. ',
   },
   {
     title: 'Instagram Saved Images Downloader',
@@ -75,7 +75,7 @@ const data = [
     link: 'https://www.reddit.com/r/Python/comments/gs54y9/made_a_telegram_bot_that_logs_into_my_instagram/',
     date: '2019-05-15',
     desc:
-      "A Telegram Bot built with Python that logged into my account and fetched my saved images. Nothing too fancy. I had hosted it somewhere earlier but it isn't hosted anywhere anymore." +
+      "A Telegram Bot built with Python that logged into my account and fetched my saved images. Nothing too fancy. I had hosted it somewhere earlier but it isn't hosted anywhere anymore. " +
       'Click on the image to see a small video of how it works.',
   },
 ];
