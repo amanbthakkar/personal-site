cdfe71a0248284553bbefc2d4f89e8877acb9af7
Aman Thakkar <aman.thakkar@somaiya.edu>
2023-12-06 12:51:11 -0800

    Readme updated

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -4,12 +4,17 @@ import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 const PageNotFound = () => (
   <HelmetProvider>
-    <div className="not-found">
-      <Helmet title="404 Not Found">
-        <meta name="description" content="The content you are looking for cannot be found." />
+    <div className='not-found'>
+      <Helmet title='404 Not Found'>
+        <meta
+          name='description'
+          content='The content you are looking for cannot be found. Please try again later.'
+        />
       </Helmet>
       <h1>Page Not Found</h1>
-      <p>Return <Link to="/">home</Link>.</p>
+      <p>
+        Return <Link to='/'>home</Link>.
+      </p>
     </div>
   </HelmetProvider>
 );
